e6d79f4ec077384fca199a388aee553b2b2872f4
semih.urtekin <semih.urtekin@johnsonelectric.com>
2026-08-26 10:39:08 +0300

    Unnecessary files have been deleted.

---
+++ main.h
@@ -0,0 +1,18 @@
+/**
+ * @file    main.h
+ * @brief   main declarations.
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==============================================================================
+ * Public Function Declarations
+ *============================================================================*/
+
+
+#endif /* MAIN_H */
